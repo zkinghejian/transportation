3f75f2ea781c11c0bc313066dab9fd7915eadfaa
hejian <651207511@qq.com>
2018-04-18 00:28:32 +0800

    UserMapperTest

--- src/main/java/com/test/TestUserMapper.java
+++ src/main/java/com/test/TestUserMapper.java
@@ -9,9 +9,7 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 import com.trans.mapper.UserMapper;
 import com.trans.pojo.User;
